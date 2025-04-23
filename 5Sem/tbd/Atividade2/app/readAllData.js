5727a2944bce9894b96e35ac70e4c2cff4e3b996
Alexandre Araújo <alexandrela37@gmail.com>
2025-04-23 17:25:24 -0300

    Adicionado material da atividade 2 de topicos em banco de dados

---
+++ 5Sem/tbd/Atividade2/app/readAllData.js
@@ -0,0 +1,26 @@
+import * as fb from 'firebase/database' // sera preciso usar fb antes de cada funcao
+import { dbConnect } from './connetToFB.js'
+
+dbConnect()
+  .then(db => {
+    //db contem a referencia ao banco
+    console.log(db) //mostra informacoes da conexao(pode excluir)
+    const dbRef = fb.ref(db, 'produtos')
+    fb.get(dbRef)
+      .then(snapshot => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val())
+        } else {
+          console.log('Não há dados disponíveis')
+        }
+      })
+      .catch(error => {
+        console.error(error)
+      })
+      .finally(() => {
+        console.log('Conexão encerrada')
+        process.exit(0)
+      }
+    )
+  })
+  .catch(err => console.log(err))
